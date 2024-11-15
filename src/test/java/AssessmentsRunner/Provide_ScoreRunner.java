dd1b6ef094dcb0799c8872281b2a195c8c79ead2
SahooSuryakanta <SahooSuryakanta@Sapphirus-SS143>
2024-11-15 11:31:32 +0530

    Written the script for provide score in Assessment Speed garder screen

--- src/test/java/AssessmentsRunner/Provide_ScoreRunner.java
+++ src/test/java/AssessmentsRunner/Provide_ScoreRunner.java
@@ -3,14 +3,15 @@ package AssessmentsRunner;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "\\src\\test\\resources\\AssessmentsFeatures\\Provide_Score.feature",
+@CucumberOptions(features = "src\\test\\resources\\AssessmentsFeatures\\Provide_Score.feature",
 glue = {"AssessmentsStepdef","Hooks"},
 plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 monochrome = true,
 dryRun = false)
-public class Provide_ScoreRunner {
+public class Provide_ScoreRunner  extends AbstractTestNGCucumberTests  {
 	
 	
 	String TestcaseName;
