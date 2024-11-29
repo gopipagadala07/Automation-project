a15aad4322d14cd58b2d8c813dc4be4218eb100d
gopi.pagadala <GopiPagadala@Sapphirus-SS141>
2024-11-29 13:53:41 +0530

    Test Data Updating

--- src/main/java/com/Utils/Base.java
+++ src/main/java/com/Utils/Base.java
@@ -55,7 +55,7 @@ public class Base {
      		options.addArguments("--headless");
 //			//options.addArguments("--window-size=1920,1080");
 			options.addArguments("--remote-allow-origins=*");
-			tlDriver.set(new ChromeDriver(options));
+			tlDriver.set(new ChromeDriver());
 		} else if (browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			tlDriver.set(new FirefoxDriver());
